7a4dc0582df024bab4beae781f50020438df8ccb
buketk <61161607+buketk@users.noreply.github.com>
2021-03-04 23:58:05 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Buket";
+            person1.LastName = "Karaçaylı";
+            person1.DateOfBirthYear = 1994;
+            person1.NationalIdentity = 50452028152;
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+    }
+
+}
